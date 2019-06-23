d607007363ebbdb9a6a113bb5dbbb0c5ac768aeb
djjproject <djj9404@gmail.com>
2019-06-24 01:25:19 +0900

    Update showdownreset.sh

--- showdownreset.sh
+++ showdownreset.sh
@@ -8,11 +8,13 @@ while true; do
     sleep 5
     SYSTEMTIME=`date "+%s"`
     SHOWDOWNTIME=`date -d "$(tail -n 1 /home/showdown/ShowDown.log | awk '{print $2}' | awk -F "," '{print $1}')" "+%s"`
+    DIFF=`expr $SYSTEMTIME - $SHOWDOWNTIME`
 
     echo "showdown time : $SHOWDOWNTIME"
     echo "system time : $SYSTEMTIME"
+    echo "diff : $DIFF"
 
-    if [ `expr $SHOWDOWNTIME + 600` -le $SYSTEMTIME ]; then
+    if [ $DIFF -gt "600" ]; then
         /usr/local/bin/showdownreset
         echo "showdownreset excuted..."
     fi
